56e901d05e89da271fb088122823de0626078b5c
EnDash <danieljshamir@gmail.com>
2016-08-01 23:48:17 +0300

    Initial Commit
    by Daniel

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(0, "Hello World!", "Message Box", 0);
+	return 0;
+}
